31f5978b241777b0957f933d086add56d122ef63
hari karthick <harikarthickr20@gmail.com>
2026-04-21 16:20:33 +0530

    Add: initialize gambler stake and bet amount

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,5 +1,15 @@
 public class GamblingSimulator {
+    static final int STAKE = 100;
+    static final int BET = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Problem");
+        startGambling();
+    }
+
+    public static void startGambling() {
+        int cash = STAKE;
+        System.out.println("Starting cash: $" + cash);
+        System.out.println("Bet per game: $" + BET);
     }
 }
